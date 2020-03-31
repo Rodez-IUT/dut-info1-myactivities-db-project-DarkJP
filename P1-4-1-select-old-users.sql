f3fa53265eef386276c74678b065b57ca50cc5e2
Ronan <ronan.bernauer2@gmail.com>
2020-03-31 17:51:57 +0200

    fix #1.1 requete P1-4-1 sur user
    Fixed a typo mistake (data_created > date_created)

--- P1-4-1-select-old-users.sql
+++ P1-4-1-select-old-users.sql
@@ -2,5 +2,5 @@
 -- avant le 1er septembre 2019 triés par ordre alphabétique sur les "username"
 SELECT "username"
 FROM "user"
-WHERE data_created < '2019-09-01 23:59:59.000000'
+WHERE date_created < '2019-09-01 23:59:59.000000'
 ORDER BY username;
